5f598cfbc1ff9c709c2e951de688482152ea8b58
SStreich <selene.streich@gmail.com>
2019-07-03 12:17:49 +0200

    Add more testing cases

--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -4,14 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ScrabbleTest {
 
+
+    @Test
+    void checkScoresForLowercaseWord() {
+        Scrabble scrabble = new Scrabble("cabbage");
+        assertEquals(14, scrabble.getScore());
+    }
+
+    @Test
+    void  checkScoresForUppercaseWord() {
+        Scrabble scrabble = new Scrabble("BANANA");
+        assertEquals(8, scrabble.getScore());
+    }
+
+
     @Test
     void checkWordTestingForNumbers(){
         assertThrows(IllegalArgumentException.class, () -> new Scrabble("1"));
     }
 
     @Test
-    void checkScores() {
-        Scrabble scrabble = new Scrabble("cabbage");
-        assertEquals(14, scrabble.getScore());
+    void checkWordTestingForNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Scrabble(null));
     }
+
+    @Test
+    void checkWordTestingForSpace() {
+        assertThrows(IllegalArgumentException.class, () -> new Scrabble(" "));
+    }
+
+
 }
